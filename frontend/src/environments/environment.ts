4f867f6aa633f1eb86229f60f465a8aa51c518dc
Charles M Dellinger <charles.dellinger@gmail.com>
2026-04-18 19:33:51 -0400

    Update environment.ts
    changed apiUrl to the url of the actual azure deployment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: 'http://localhost:5083',
+    apiUrl: 'snipurl-fuejbfhnh0fgeyem.westus2-01.azurewebsites.net',
     googleClientId: '588096585312-1bq8613drp922hurg10l0mu234bui4cd.apps.googleusercontent.com',
 };
